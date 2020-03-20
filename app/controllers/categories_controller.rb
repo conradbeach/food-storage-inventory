295aef2b418f3c92c775ba69d153b2ca0026eec3
Conrad Beach <conradbeach@protonmail.com>
2020-03-20 15:54:51 -0600

    feature: Improve categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,12 @@
 class CategoriesController < ApplicationController
+  before_action :require_login
   before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_action :scope_to_current_user, only: [:show, :edit, :update, :destroy]
 
   # GET /categories
   # GET /categories.json
   def index
-    @categories = Category.all
+    @categories = current_user.categories
   end
 
   # GET /categories/1
@@ -15,16 +17,19 @@ class CategoriesController < ApplicationController
   # GET /categories/new
   def new
     @category = Category.new
+    @storage_types = current_user.storage_types
   end
 
   # GET /categories/1/edit
   def edit
+    @storage_types = current_user.storage_types
   end
 
   # POST /categories
   # POST /categories.json
   def create
     @category = Category.new(category_params)
+    @storage_types = current_user.storage_types
 
     respond_to do |format|
       if @category.save
@@ -65,6 +70,10 @@ class CategoriesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_category
       @category = Category.find(params[:id])
+    end
+
+    def scope_to_current_user
+      redirect_to sign_in_path unless @category.storage_type.user == current_user
     end
 
     # Only allow a list of trusted parameters through.
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   include Clearance::User
 
   has_many :storage_types, dependent: :restrict_with_exception
+  has_many :categories, through: :storage_types
 end
