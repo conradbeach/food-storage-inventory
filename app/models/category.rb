90e866ac885aed99f8be9cc2f5bd58ca1abc55f0
Conrad Beach <conradbeach@protonmail.com>
2020-03-27 14:25:15 -0600

    feature: Display number of years provided for per category

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,5 +4,17 @@ class Category < ApplicationRecord
 
   enum unit_type: [:pounds, :servings, :quantity]
 
+  delegate :user, to: :storage_type
+
   validates_presence_of :name, :unit_type, :units_per_year_per_adult
+
+  def total_units
+    food_items.sum(:units)
+  end
+
+  def number_of_years_provided_for
+    return 0 if user.adults_providing_for.blank? || user.adults_providing_for.zero?
+
+    total_units / units_per_year_per_adult / user.adults_providing_for
+  end
 end
