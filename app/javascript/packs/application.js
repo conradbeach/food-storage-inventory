513dfc42e76f20572004e6cc39f37a458b5f6a0e
Conrad Beach <conradbeach@protonmail.com>
2020-05-12 12:04:17 -0600

    chore: Install Action Text
    Installed by running `bin/rails action_text:install` and `rails db:migrate`.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,3 +18,6 @@ require('@fortawesome/fontawesome-free/js/all');
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require("trix")
+require("@rails/actiontext")
