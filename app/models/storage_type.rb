dcc8ab6f70869244a87e2fbca2772b4879083baf
Conrad Beach <conradbeach@protonmail.com>
2020-08-28 15:39:29 -0600

    feature: Prevent deleting categories and storage types with child records
    I don't want anyone to accidentally delete a huge number of items. 
    There's definitely more sophisticated ways we could handle
    this and better UX we could offer. This is a good start though.

--- app/models/storage_type.rb
+++ app/models/storage_type.rb
@@ -3,5 +3,5 @@
 class StorageType < ApplicationRecord
   belongs_to :user
 
-  has_many :categories, dependent: :restrict_with_exception
+  has_many :categories, dependent: :restrict_with_error
 end
