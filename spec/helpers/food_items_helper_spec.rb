e2313b30e553e5440528e8b89bca760b50d58a01
Conrad Beach <conradbeach@protonmail.com>
2020-09-05 06:28:59 -0600

    feature: Add format_units helper
    This helper method displays weight in a more user-friendly way. It's also
    intended to display weight in the same way food packaging usually does.

--- spec/helpers/food_items_helper_spec.rb
+++ spec/helpers/food_items_helper_spec.rb
@@ -2,16 +2,54 @@
 
 require "rails_helper"
 
-# Specs in this file have access to a helper object that includes
-# the FoodItemsHelper. For example:
-#
-# describe FoodItemsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe FoodItemsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#format_units" do
+    context "when the unit type is pounds" do
+      context "when the weight is less than 4 pounds" do
+        it "returns the weight in ounces" do
+          expect(format_units(0.5, "pounds")).to eq("8 ounces")
+          expect(format_units(2.5, "pounds")).to eq("40 ounces")
+          expect(format_units(3.75, "pounds")).to eq("60 ounces")
+          expect(format_units(3.333, "pounds")).to eq("53.33 ounces")
+        end
+
+        context "when the weight is a whole number" do
+          it "returns the weight as a whole number" do
+            expect(format_units(1, "pounds")).to eq("1 pound")
+            expect(format_units(2, "pounds")).to eq("2 pounds")
+            expect(format_units(3, "pounds")).to eq("3 pounds")
+          end
+        end
+      end
+
+      context "when the weight is greater than or equal to 4 pounds" do
+        it "returns the weight in pounds" do
+          expect(format_units(4, "pounds")).to eq("4 pounds")
+          expect(format_units(45, "pounds")).to eq("45 pounds")
+          expect(format_units(5.25, "pounds")).to eq("5.25 pounds")
+          expect(format_units(12.3921, "pounds")).to eq("12.39 pounds")
+        end
+      end
+    end
+
+    context "when the unit type is servings" do
+      it "returns servings" do
+        expect(format_units(10, "servings")).to eq("10 servings")
+      end
+
+      it "returns singular servings" do
+        expect(format_units(1, "servings")).to eq("1 serving")
+      end
+    end
+
+    context "when the unit type is quantity" do
+      it "returns units" do
+        expect(format_units(10, "quantity")).to eq("10 units")
+      end
+
+      it "returns singular units" do
+        expect(format_units(1, "quantity")).to eq("1 unit")
+      end
+    end
+  end
 end
