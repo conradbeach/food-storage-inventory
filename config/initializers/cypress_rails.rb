29825ef4192a3d5b087ab700a4c00b27842805ff
Conrad Beach <conradbeach@protonmail.com>
2020-09-24 11:34:51 -0600

    tests: Load dummy data before running Cypress tests

---
+++ config/initializers/cypress_rails.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+return unless Rails.env.test?
+
+CypressRails.hooks.before_server_start do
+  # Called once, before either the transaction or the server is started
+end
+
+CypressRails.hooks.after_transaction_start do
+  # Called after the transaction is started (at launch and after each reset)
+  FoodStorageInventory::Application.load_tasks
+  Rake::Task["db:add_dummy_data"].invoke
+end
+
+CypressRails.hooks.after_state_reset do
+  # Triggered after `/cypress_rails_reset_state` is called
+end
+
+CypressRails.hooks.before_server_stop do
+  # Called once, at_exit
+end
