edd35fe89925254dcabd1697a57d911df9935be6
Conrad Beach <conradbeach@protonmail.com>
2021-01-28 12:12:06 -0700

    chore: Remove turbolinks reference
    We removed the turbolinks package when we installed turbo-rails.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,6 @@
 import '../stylesheets/application.scss';
 
 require('@rails/ujs').start();
-require('turbolinks').start();
 require('@rails/activestorage').start();
 require('channels'); // eslint-disable-line import/no-unresolved
 
