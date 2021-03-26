8fe8715d6e23c2091f87d764dd492a0361887c6d
Conrad Beach <conradbeach@protonmail.com>
2021-03-26 13:21:08 -0600

    style: Use singlequotes in TableController

--- app/javascript/controllers/table_controller.js
+++ app/javascript/controllers/table_controller.js
@@ -1,14 +1,14 @@
 import { Controller } from 'stimulus';
 
 export default class TableController extends Controller {
-  static targets = ["groupChild"];
+  static targets = ['groupChild'];
 
   toggleGroup(event) {
     const { groupName } = event.target.parentElement.dataset;
 
     this.groupChildTargets.forEach((child) => {
       if (groupName === child.dataset.groupName) {
-        child.classList.toggle("hidden");
+        child.classList.toggle('hidden');
       }
     });
   }
